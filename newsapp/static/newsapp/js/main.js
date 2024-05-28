587b94914b18e94d1435dcbcba207cce14957f53
Nevin Aquinas <87398867+neaqx@users.noreply.github.com>
2024-05-28 13:52:20 +0000

    Update main.js to fix ERROR 426

--- newsapp/static/newsapp/js/main.js
+++ newsapp/static/newsapp/js/main.js
@@ -1,16 +1,21 @@
 
-var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=63ae134ce98b4f88bf9bc68906a86db9?' +
-    'q=Apple&' +
-    'from=2024-05-28&' +
-    'sortBy=popularity&' +
-    'apiKey=63ae134ce98b4f88bf9bc68906a86db9';
+const fetchNews = async () => {
+    console.log('fetching news')
+    var url = 'https://newsapi.org/v2/everything?q=Apple&from=2024-05-28&sortBy=popularity&apiKey=API_KEY' +
+        'q=Apple&' +
+        'from=2024-05-28&' +
+        'sortBy=popularity&' +
+        'apiKey=63ae134ce98b4f88bf9bc68906a86db9';
 
-var req = new Request(url);
+    var req = new Request(url);
 
-fetch(req)
-    .then(function (response) {
-        console.log(response.json());
-    })
+    let a = await fetch(req)
+    let response = await a.json()
+    console.log(JSON.stringify(Response, null, 2))
+}
+
+
+fetchNews()
 
 
 
